a853e0ab3113073854c2d0cc433933bf6c2b8118
janezhou309645512 <39550832+janezhou309645512@users.noreply.github.com>
2018-11-19 15:18:11 +0800

    2018-11-19 2

---
+++ WebMvc/Areas/Web/Controllers/FileController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace WebMvc.Areas.Web.Controllers
+{
+    public class FileController : Controller
+    {
+       
+        // GET: Web/File
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+
+
+
+
+            return View();
+        }
+        public string UploadFile(HttpPostedFileBase file)
+        {
+
+           
+            return Dal.FileOperation.UploadFile(file);
+        }
+
+
+        //-rw-r--r-- 1 ftp ftp 13158 Nov 17 13:34 a.png        -rw-r--r-- 1 ftp ftp 13158 Nov 18 14:57 abcd.png-rw-r--r-- 1 ftp ftp 0 Nov 18 14:51 chrome.exe.sig-rw-r--r-- 1 ftp ftp 0 Nov 18 14:46 chrome_100_percent.pak-rw-r--r-- 1 ftp ftp 436 Nov 15 2017 StudyDay.txtdrwxr-xr-x 1 ftp ftp 0 Nov 18 15:02 UserImage-rw-r--r-- 1 ftp ftp 168 Nov 17 12:34 web.config-rw-r--r-- 1 ftp ftp 2734 Nov 17 13:31 鍗楁嫵濡堝 - 涓嬮洦澶?896b9ecf93cacce134b1f8d537123287-26615577-00000000.krc-rw-r--r-- 1 ftp ftp 60718 Nov 17 13:35 鎴浘00.png-rw-r--r-- 1 ftp ftp 16029 Nov 17 13:41 姣忔棩鏁版嵁姹囨姤.xlsx-rw-r--r-- 1 ftp ftp 907734 Nov 17 13:34 鑻遍泟鏃跺埢_20171008-05鐐?5鍒?2s.avi
+        public string AllFiles()
+        {
+          return Dal.FileOperation.AllFiles();
+        }
+
+
+
+
+    }
+}
---
+++ WebMvc/Areas/Web/Dal/FileOperation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Web.Models;
+
+namespace WebMvc.Areas.Web.Dal
+{
+   
+    public class FileOperation
+    {
+        static  FtpClient ftp = new FtpClient("120.79.146.123", "jane.zhou1", "309645512");
+        public static string UploadFile(HttpPostedFileBase file)
+        {
+          bool a=  ftp.WebUpload(file);
+            if (a)
+            {
+
+                return "OK";
+
+
+            }else
+            {
+
+                return "NO";
+            }
+
+
+            
+
+        }
+        public static string AllFiles()
+        {
+            List<string> ftpFiles = ftp.GetFileDetails();
+            string str = "";
+            for (int i = 0; i < ftpFiles.Count; i++)
+            {
+
+                str += ftpFiles[i];
+
+
+
+            }
+
+
+
+            return str;
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
